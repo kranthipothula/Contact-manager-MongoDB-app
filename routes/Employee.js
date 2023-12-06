107430a2efe5d9469edd60a27fcf48d1bbd2428a
kranthipothula <150351636+kranthipothula@users.noreply.github.com>
2023-12-06 17:39:00 +0530

    Backed mongodb

--- routes/Employee.js
+++ routes/Employee.js
@@ -7,6 +7,6 @@ router.get('/', EmployeeController.index)
  router.post('/show', EmployeeController.show)
  router.post('/store', EmployeeController.store)
  router.post('/update', EmployeeController.update)
-//router.post('/delete', EmployeeController.delete)
+router.post('/delete', EmployeeController.delete)
 
 module.exports = router
